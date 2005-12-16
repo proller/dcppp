b9994c3e3df51c292b6a0e32c01788b0ef0f99b3
pro <pro@5a96b049-0f08-0410-82a6-d9675ca652f6>
2005-12-16 19:48:46 +0000

    headers:

--- trunk/dcppp.pm
+++ trunk/dcppp.pm
@@ -1,1 +1,25 @@
+#!/usr/bin/perl
+my $Id = '$Id: crawler.pl 1702 2005-12-15 14:36:54Z pro $';
+=copyright
+dcpp for perl 
+Copyright (C) 2003-2005 !!CONTACTS HERE!!
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA,
+or download it from http://www.gnu.org/licenses/gpl.html
+=cut
+
+  use strict;
+
 
