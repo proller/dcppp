9b690aecc4bb9de2cf8bbc253f68643e64b5c2bb
pro <pro@5a96b049-0f08-0410-82a6-d9675ca652f6>
2009-10-12 03:51:35 +0000

    adc progress..

--- trunk/examples/watch.pl
+++ trunk/examples/watch.pl
@@ -13,10 +13,11 @@ use lib '../lib';
 use Net::DirectConnect::clihub;
 print("usage: flood.pl [dchub://]host[:port] [bot_nick]\n"), exit if !$ARGV[0];
 #print "Arg=",$ARGV[0],"\n";
-$ARGV[0] =~ m|^(?:dchub\://)?(.+?)(?:\:(\d+))?$|;
+#$ARGV[0] =~ m|^(?:\w+\://)?(.+?)(?:\:(\d+))?$|;
 my $dc = Net::DirectConnect::clihub->new(
-  'host' => $1,
-  ( $2 ? ( 'port' => $2 ) : () ),
+  #'host' => $1,
+  'host' => $ARGV[0],
+  #( $2 ? ( 'port' => $2 ) : () ),
   'Nick' => ( $ARGV[1] or int( rand(100000000) ) ),
   #'Nick'		=>	'xxxx',
   'sharesize' => int( rand 100000000000 ) + int( rand 10000000000 ) * int( rand 100 ),
