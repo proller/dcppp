f4eb36ffe0fc9ff586faa5d9aa420786c3101e6d
pro <proler@gmail.com>
2009-10-25 01:51:26 +0000

    TigerHash port from linuxdcpp

---
+++ lib/Net/DirectConnect/TigerHash/lib/TigerHash.pm
@@ -0,0 +1,95 @@
+package TigerHash;
+
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use tiger ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+tthbin
+tth
+) ] );
+#	tigerhash
+
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+our $VERSION = '0.01';
+
+require XSLoader;
+XSLoader::load('TigerHash', $VERSION);
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+tiger - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use tiger;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for tiger, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+=head2 Exportable functions
+
+  void shutdown()
+  void startup(void (*f)(void*, const string&), void* p)
+  void tigerhash(void * data, int len, void * result)
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Charlie &, E<lt>root@setun.netE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by Charlie &
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
