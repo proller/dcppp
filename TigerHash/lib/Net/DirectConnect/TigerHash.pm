86dbdf477abb872be28bd2ca837c129425be0391
pro <proler@gmail.com>
2010-01-25 21:36:54 +0000

    0.02

--- TigerHash/lib/Net/DirectConnect/TigerHash.pm
+++ TigerHash/lib/Net/DirectConnect/TigerHash.pm
@@ -1,35 +1,18 @@
 #$Id$ $URL$
 package Net::DirectConnect::TigerHash;
-our $VERSION = '0.01' . '_' . ( split( ' ', '$Revision$' ) )[1];
+our $VERSION = '0.02';# . '_' . ( split( ' ', '$Revision$' ) )[1];
 use 5.006001;
 use strict;
 use warnings;
 require Exporter;
 our @ISA = qw(Exporter);
-#Items to export into callers namespace by default. Note: do not export
-#names by default without a very good reason. Use EXPORT_OK instead.
-#Do not simply export all your public functions/methods/constants.
-#This allows declaration	use tiger ':all';
-#If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-#will save memory.
-our %EXPORT_TAGS = (
-  'all' => [ qw(
-      tthbin
-      tth
-      tthfile
-      )
-  ]
-);
-#tigerhash
+our %EXPORT_TAGS = ( 'all' => [ qw( tthbin tth tthfile ) ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-our @EXPORT    = qw(
-);
+our @EXPORT    = qw( );
 require XSLoader;
 XSLoader::load( 'Net::DirectConnect::TigerHash', $VERSION );
-#Preloaded methods go here.
 1;
 __END__
-#Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
