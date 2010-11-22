7770848127fe936eb859bf72a6fb4e9e295b161d
pro <proler@gmail.com>
2010-11-22 12:18:41 +0000

    mingw fix

--- TigerHash/dcpp/DCPlusPlus.h
+++ TigerHash/dcpp/DCPlusPlus.h
@@ -99,16 +99,6 @@ typedef std::pair<wstring, wstring> WStringPair;
 typedef std::vector<WStringPair> WStringPairList;
 typedef WStringPairList::iterator WStringPairIter;
 
-/*typedef unsigned char uint8_t;
-typedef signed char int8_t             ;
-typedef unsigned char uint8_t          ;
-typedef signed int int16_t             ;
-typedef unsigned int uint16_t          ;
-typedef signed long int int32_t        ;
-typedef unsigned long int uint32_t     ;
-typedef signed long long int int64_t   ;
-typedef unsigned long long int uint64_t;
-*/
 
 typedef std::vector<uint8_t> ByteVector;
 /*
