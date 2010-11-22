673d08a727e1da263b31a894a356f24a27cf9b36
pro <pro@5a96b049-0f08-0410-82a6-d9675ca652f6>
2010-11-22 12:18:41 +0000

    mingw fix

--- trunk/TigerHash/os.h
+++ trunk/TigerHash/os.h
@@ -5,28 +5,24 @@
 
 	//#if (defined(_WIN32) || defined(__WIN32__)) && ( !defined(__CYGWIN__) && !defined(__CYGWIN64__) && !defined(__CYGWIN32__) )
 	#if ( defined(__WIN32__)) && ( !defined(__CYGWIN__) && !defined(__CYGWIN64__) && !defined(__CYGWIN32__) )
-		typedef signed __int64 int64_t;
-		typedef unsigned __int8 uint8_t;
+
+typedef signed __int8 int8_t;
+typedef signed __int16 int16_t;
+typedef signed __int32 int32_t;
+typedef signed __int64 int64_t;
+
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+
 	#endif
 
 	#if ( defined(__WIN64__) && !defined(__CYGWIN64__))
 		typedef struct stat64 STAT;
-		//#define stat64 STAT
 	#else
 		typedef struct stat STAT;
-		//#define stat STAT
 	#endif
 
-/*
-typedef unsigned char uint8_t;
-typedef signed char int8_t             ;
-typedef unsigned char uint8_t          ;
-typedef signed int int16_t             ;
-typedef unsigned int uint16_t          ;
-typedef signed long int int32_t        ;
-typedef unsigned long int uint32_t     ;
-typedef signed long long int int64_t   ;
-typedef unsigned long long int uint64_t;
-*/
 
 #endif
