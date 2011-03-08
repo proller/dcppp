f043eadbe9997e5354979936abf139dfcdcd93b3
pro <proler@gmail.com>
2011-03-08 18:47:54 +0000

    0.06

--- TigerHash/lib/Net/DirectConnect/TigerHash.pm
+++ TigerHash/lib/Net/DirectConnect/TigerHash.pm
@@ -1,6 +1,6 @@
 #$Id$ $URL$
 package Net::DirectConnect::TigerHash;
-our $VERSION = '0.05';# . '_' . ( split( ' ', '$Revision$' ) )[1];
+our $VERSION = '0.06';# . '_' . ( split( ' ', '$Revision$' ) )[1];
 use 5.006001;
 use strict;
 use warnings;
